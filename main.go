aae19e1ee36fc7c42b72856f1862936b73310e0b
Andrew Hoglund <ahoglund@github.com>
2023-01-28 12:49:26 -0600

    Super basic functionality.
    This can parse templates and return a slice of strings.

--- main.go
+++ main.go
@@ -10,16 +10,21 @@ import (
 	"github.com/gomarkdown/markdown"
 )
 
+type FrontMatter struct {
+	title    string
+	template string
+	content  []byte
+}
+
 const templateDir = "example/templates"
 const pagesDir = "example/pages"
 const frontMatterDelimiter = "---\n"
 const defaultTemplate = "index"
 
+// iterate through every page
+// and write out the html version
+// to the public directory.
 func main() {
-	// iterate through every page
-	// and write out the html version
-	// to the public directory.
-
 	// Get all .md files in pagesDir
 	dirs, err := ioutil.ReadDir(pagesDir)
 	if err != nil {
@@ -39,13 +44,13 @@ func main() {
 			data = append(data, line)
 		}
 
-		frontMatter := data[1]
+		rawFrontMatter := data[1]
 		markdownContent := data[2]
 		// Need to ignore comments in front matter.
 
 		config := make(map[string]string)
 
-		for _, line := range strings.Split((frontMatter), "\n") {
+		for _, line := range strings.Split((rawFrontMatter), "\n") {
 			if line == "" {
 				continue
 			}
@@ -64,30 +69,51 @@ func main() {
 			os.Exit(1)
 		}
 
-		parsedTemplateContent := parseTemplate(templateContent, config["title"], markdown.ToHTML([]byte(markdownContent), nil, nil))
+		frontMatter := &FrontMatter{
+			title:    config["title"],
+			template: config["template"],
+			content:  markdown.ToHTML([]byte(markdownContent), nil, nil),
+		}
+
+		parsedTemplateContent := parseTemplate(templateContent, frontMatter)
 		fmt.Println(parsedTemplateContent)
 	}
 
 }
 
 // parseTemplate takes in a template containing {{ }} variable strings and replaces them
-// with either the templates refered to (by t:<string>) or variables (by v:<string>) in the config map.
+// with either the templates refered to (by t:<string>) in the config map.
 // content is a special variable.
 // title is a special variable.
-func parseTemplate(templateContent []byte, title string, content []byte) []string {
+func parseTemplate(templateContent []byte, frontMatter *FrontMatter) []string {
 	parsedTemplateContent := make([]string, 0)
 	for _, line := range strings.Split(string(templateContent), "\n") {
 		r := regexp.MustCompile(`.*{{\s*(.*)\s*}}.*`)
 		if r.Match([]byte(line)) {
 			found := r.FindAllStringSubmatch(line, -1)
-			for _, m := range found {
-				fmt.Println(m[1])
+			v := strings.Split(found[0][1], ":")
+
+			// I need to not trim space so much!
+			varName := strings.TrimSpace(v[0])
+			switch varName {
+			case "t":
+				// load the template and then add it to the parsedTemplateContent
+				templateName := strings.TrimSpace(v[1])
+				templateContent, err := ioutil.ReadFile(templateDir + "/" + templateName + ".html.template")
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%v\n", err)
+					os.Exit(1)
+				}
+				subTemplateContent := parseTemplate(templateContent, frontMatter)
+				parsedTemplateContent = append(parsedTemplateContent, subTemplateContent...)
+			case "content":
+				parsedTemplateContent = append(parsedTemplateContent, string(frontMatter.content))
+			case "title":
+				parsedTemplateContent = append(parsedTemplateContent, string(frontMatter.title))
+			default:
+				fmt.Fprintf(os.Stderr, "Unsupported variable: %s", v[0])
+				os.Exit(1)
 			}
-			// templateContent, err := ioutil.ReadFile(templateDir + "/" + templateName + ".html.template")
-			// if err != nil {
-			// 	fmt.Fprintf(os.Stderr, "%v\n", err)
-			// 	os.Exit(1)
-			// }
 		} else {
 			parsedTemplateContent = append(parsedTemplateContent, line)
 		}
